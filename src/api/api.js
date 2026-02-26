3304d6f8154758e2449a636a17e8fd3a4b46bedc
sao333167 <sao333167@gmail.com>
2026-02-26 12:38:06 +0700

    project update api

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const local = 'http://localhost:5000'
-const production = 'https://backend-multi-vendor-ecommerce-zoxo.onrender.com'
+const production = 'https://backend-multi-vendor-ecommerce-1.onrender.com'
 
 let api_url = ''
 let mode = 'pro'
